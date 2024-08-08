ad4a7fa92f1b02678e52f39bf2c03dfc8ebce14d
autologon <autologon@SP01-306-39>
2024-08-08 11:58:45 -0300

    Cadastro De Eventos -> Feito

---
+++ Evento.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace Programa_De_Evento__Antônio_
+{
+    partial class Evento
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Orçamento = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.atrações = new System.Windows.Forms.Label();
+            this.Nome_evento = new System.Windows.Forms.Label();
+            this.Hora = new System.Windows.Forms.Label();
+            this.Data = new System.Windows.Forms.Label();
+            this.Pessoas = new System.Windows.Forms.Label();
+            this.Local = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.criarEvento_button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Orçamento
+            // 
+            this.Orçamento.AutoSize = true;
+            this.Orçamento.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Orçamento.Location = new System.Drawing.Point(18, 21);
+            this.Orçamento.Name = "Orçamento";
+            this.Orçamento.Size = new System.Drawing.Size(106, 21);
+            this.Orçamento.TabIndex = 0;
+            this.Orçamento.Text = "Orçamento:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(22, 45);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(258, 27);
+            this.textBox1.TabIndex = 1;
+            // 
+            // atrações
+            // 
+            this.atrações.AutoSize = true;
+            this.atrações.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.atrações.Location = new System.Drawing.Point(440, 305);
+            this.atrações.Name = "atrações";
+            this.atrações.Size = new System.Drawing.Size(86, 21);
+            this.atrações.TabIndex = 2;
+            this.atrações.Text = "Atrações:";
+            // 
+            // Nome_evento
+            // 
+            this.Nome_evento.AutoSize = true;
+            this.Nome_evento.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Nome_evento.Location = new System.Drawing.Point(440, 110);
+            this.Nome_evento.Name = "Nome_evento";
+            this.Nome_evento.Size = new System.Drawing.Size(146, 21);
+            this.Nome_evento.TabIndex = 3;
+            this.Nome_evento.Text = "Nome do Evento:";
+            // 
+            // Hora
+            // 
+            this.Hora.AutoSize = true;
+            this.Hora.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Hora.Location = new System.Drawing.Point(440, 207);
+            this.Hora.Name = "Hora";
+            this.Hora.Size = new System.Drawing.Size(51, 21);
+            this.Hora.TabIndex = 4;
+            this.Hora.Text = "Hora:";
+            // 
+            // Data
+            // 
+            this.Data.AutoSize = true;
+            this.Data.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Data.Location = new System.Drawing.Point(21, 207);
+            this.Data.Name = "Data";
+            this.Data.Size = new System.Drawing.Size(55, 21);
+            this.Data.TabIndex = 5;
+            this.Data.Text = "Data:";
+            // 
+            // Pessoas
+            // 
+            this.Pessoas.AutoSize = true;
+            this.Pessoas.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Pessoas.Location = new System.Drawing.Point(21, 305);
+            this.Pessoas.Name = "Pessoas";
+            this.Pessoas.Size = new System.Drawing.Size(119, 21);
+            this.Pessoas.TabIndex = 7;
+            this.Pessoas.Text = "N° de Pessoas:";
+            // 
+            // Local
+            // 
+            this.Local.AutoSize = true;
+            this.Local.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Local.Location = new System.Drawing.Point(21, 110);
+            this.Local.Name = "Local";
+            this.Local.Size = new System.Drawing.Size(55, 21);
+            this.Local.TabIndex = 8;
+            this.Local.Text = "Local:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(440, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 21);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Fornecedor:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(443, 228);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(238, 27);
+            this.textBox2.TabIndex = 10;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(443, 134);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(238, 27);
+            this.textBox3.TabIndex = 11;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox4.Location = new System.Drawing.Point(24, 329);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(256, 27);
+            this.textBox4.TabIndex = 12;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox5.Location = new System.Drawing.Point(22, 231);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(258, 27);
+            this.textBox5.TabIndex = 13;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox6.Location = new System.Drawing.Point(22, 134);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(258, 27);
+            this.textBox6.TabIndex = 14;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox7.Location = new System.Drawing.Point(443, 45);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(238, 27);
+            this.textBox7.TabIndex = 15;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox8.Location = new System.Drawing.Point(443, 329);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(238, 27);
+            this.textBox8.TabIndex = 16;
+            // 
+            // criarEvento_button
+            // 
+            this.criarEvento_button.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.criarEvento_button.Location = new System.Drawing.Point(306, 386);
+            this.criarEvento_button.Name = "criarEvento_button";
+            this.criarEvento_button.Size = new System.Drawing.Size(127, 46);
+            this.criarEvento_button.TabIndex = 17;
+            this.criarEvento_button.Text = "Criar";
+            this.criarEvento_button.UseVisualStyleBackColor = true;
+            // 
+            // Evento
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.BurlyWood;
+            this.ClientSize = new System.Drawing.Size(708, 444);
+            this.Controls.Add(this.criarEvento_button);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Local);
+            this.Controls.Add(this.Pessoas);
+            this.Controls.Add(this.Data);
+            this.Controls.Add(this.Hora);
+            this.Controls.Add(this.Nome_evento);
+            this.Controls.Add(this.atrações);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Orçamento);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Evento";
+            this.Text = "Evento (Cliente)";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Orçamento;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label atrações;
+        private System.Windows.Forms.Label Nome_evento;
+        private System.Windows.Forms.Label Hora;
+        private System.Windows.Forms.Label Data;
+        private System.Windows.Forms.Label Pessoas;
+        private System.Windows.Forms.Label Local;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.Button criarEvento_button;
+    }
+}
